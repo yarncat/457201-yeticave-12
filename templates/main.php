cc0e7bdfa70c05a5a26dfc060ec0b79eb9c1ca4e
Alexandr Petrov <yarncat@yandex.ru>
2020-04-26 05:23:15 +0400

    Шаблонизация проекта и фильтрация полученных данных

---
+++ templates/main.php
@@ -0,0 +1,38 @@
+<section class="promo">
+        <h2 class="promo__title">Нужен стафф для катки?</h2>
+        <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+        <ul class="promo__list">
+            <?php foreach ($itemCategory as $value): ?>
+            <li class="promo__item promo__item--boards">
+                <a class="promo__link" href="pages/all-lots.html"><?=htmlspecialchars($value); ?></a>
+            </li>
+			<?php endforeach; ?>
+        </ul>
+    </section>
+    <section class="lots">
+        <div class="lots__header">
+            <h2>Открытые лоты</h2>
+        </div>
+        <ul class="lots__list">
+            <?php foreach ($items as $key => $value): ?>
+            <li class="lots__item lot">
+                <div class="lot__image">
+                    <img src="<?=htmlspecialchars($value['imageurl']); ?>" width="350" height="260" alt="">
+                </div>
+                <div class="lot__info">
+                    <span class="lot__category"><?=$value['category']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($value['title']); ?></a></h3>
+                    <div class="lot__state">
+                        <div class="lot__rate">
+                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__cost"><?=formatSum(htmlspecialchars($value['price'])); ?></span>
+                        </div>
+                        <div class="lot__timer timer">
+                            12:23
+                        </div>
+                    </div>
+                </div>
+            </li>
+			<?php endforeach; ?>
+        </ul>
+    </section>
